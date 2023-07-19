2c6080f2fae9c854ea2f10e81165fa09b78158f4
Emre Ulgac <ulgacemre@gmail.com>
2023-07-19 15:52:55 +0000

    Faq update

--- app/components/FAQModal.tsx
+++ app/components/FAQModal.tsx
@@ -54,7 +54,7 @@ export default function FAQModal({ onCloseModal }: Props) {
     >
       <div
         className={
-          "p-16 max-w-[50vw] bg-white bg-opacity-[4%] border-white border-[2px] rounded-lg border-opacity-10"
+          "p-16 max-w-[50vw]  bg-white bg-opacity-[4%] border-white border-[2px] rounded-lg border-opacity-10"
         }
       >
         <div className="flex justify-between mb-2">
@@ -68,23 +68,8 @@ export default function FAQModal({ onCloseModal }: Props) {
         </div>
 
         <div className="shadow-lg rounded-lg overflow-hidden mt-5">
-          <div className="overflow-x-auto overflow-y-auto max-h-80">
-            <div className='flex items-center justify-between w-full mb-6 lg:justify-end'>
-              <button
-                className="flex items-center mr-3 text-sm font-bold lg:text-base py-2 px-4"
-                onClick={handleClick}
-              >
-                <span className="min-w-fit text-ellipsis"> {!isSomeActive ? "Open All" : "Close All"}</span>
-                <div
-                  className={
-                    "ml-2 relative transition-all ease-in-out duration-200 " +
-                    (isSomeActive ? " rotate-180" : "rotate-0")
-                  }
-                >
-                  <FontAwesomeIcon icon={faAngleDown} />
-                </div>
-              </button>
-            </div>
+          <div className="overflow-x-auto overflow-y-auto max-h-90">
+
 
             {questions.map((el, i) => {
               return (
