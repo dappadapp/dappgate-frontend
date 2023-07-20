53bcdf435923aa40b2f12eb9f46218fea98ed169
Emre Ulgac <ulgacemre@gmail.com>
2023-07-20 19:59:58 +0000

    cache false

--- app/api/counter/route.ts
+++ app/api/counter/route.ts
@@ -5,7 +5,7 @@ export async function POST() {
 
 
   const refDataResponse = await axios.get(
-    `${process.env.NEXT_PUBLIC_API_URL}/counter?cache=true`
+    `${process.env.NEXT_PUBLIC_API_URL}/counter?cache=false`
   );
 
   console.log("refDataResponse",refDataResponse);
