04ce0585469d89b91d392885c5d94db278e56280
Can Özfuttu <84626499+cozfuttu@users.noreply.github.com>
2023-07-21 13:25:45 +0300

    buttons loading state & bridge error check

--- components/MintButton.tsx
+++ components/MintButton.tsx
@@ -32,6 +32,7 @@ const MintButton: React.FC<Props> = ({
   logIndex,
 }) => {
   const [mintTxHash, setMintTxHash] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const { chain: connectedChain } = useNetwork();
   const { switchNetworkAsync } = useSwitchNetwork();
@@ -123,6 +124,7 @@ const MintButton: React.FC<Props> = ({
       return alert("An unknown error occured. Please try again.");
     }
     try {
+      setLoading(true);
       if (connectedChain?.id !== sourceChain.chainId) {
         await switchNetworkAsync?.(sourceChain.chainId);
       }
@@ -131,15 +133,36 @@ const MintButton: React.FC<Props> = ({
       toast("Mint transaction sent, waiting confirmation...");
     } catch (error) {
       console.log(error);
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
     <button
       onClick={onMint}
-      className={"bg-white/10 border-white border-[1px] rounded-lg px-16 py-2"}
+      disabled={loading}
+      className={
+        "flex items-center gap-1 bg-white/10 border-white border-[1px] rounded-lg px-16 py-2"
+      }
     >
       Mint
+      {loading && (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-4 h-4 animate-spin"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+          />
+        </svg>
+      )}
     </button>
   );
 };
