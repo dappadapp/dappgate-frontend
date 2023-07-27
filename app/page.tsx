f1525cd7f021c74f86ad4b4142955f12bbe4a2c5
mustafaensar <mustafaensar55@gmail.com>
2023-07-27 20:26:34 +0200

    Footer changed to component

--- app/page.tsx
+++ app/page.tsx
@@ -54,6 +54,7 @@ import ListboxTargetMenu from "./components/ListboxTargetMenu";
 import ONFTHyperMintButton from "@/components/ONFTHyperMintButton";
 import CircleSvg from "./components/CircleSvg";
 import DappGateLogo from "./components/DappGateLogo";
+import Footer from "./components/Footer";
 
 const networks: Network[] = [
   {
@@ -1535,33 +1536,7 @@ export default function Home({
               </div>
             )}
           </div>
-          <div className={"mt-auto pb-16 flex justify-between items-center"}>
-            <p className={"text-gray-400 font-light"}>
-              DappGate by{" "}
-              <a
-                href={"https://dappad.app/"}
-                className={"text-white font-bold"}
-              >
-                DappLabs
-              </a>
-            </p>
-            <div className={"flex gap-4 text-xl text-gray-400 "}>
-              <a
-                href="https://twitter.com/Dappadofficial"
-                target="_blank"
-                className={"hover:text-gray-100 transition-all"}
-              >
-                <FontAwesomeIcon icon={faTwitter} />
-              </a>
-              <a
-                href={"https://discord.gg/dappadlaunchpad"}
-                target="_blank"
-                className={"hover:text-gray-100 transition-all"}
-              >
-                <FontAwesomeIcon icon={faDiscord} />
-              </a>
-            </div>
-          </div>
+          <Footer/>
         </div>
       </div>
       <div className={"relative w-full h-full"}>
