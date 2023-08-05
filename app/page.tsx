facb9de8ab037a01ca1828fbfde26d3078d5fa19
Emre Ulgac <ulgacemre@gmail.com>
2023-08-05 15:36:29 +0000

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -637,7 +637,7 @@ export default function Home({
   const [mintCounter, setMintCounter] = useState(0);
   const [isTransferModalOpen, setIsTransferModalOpen] = useState(false);
   const [gasRefuelAmount, setGasRefuelAmount] = useState("");
-  const [dlgateBridgeAmount, setDlgateBridgeAmount] = useState(0);
+  const [dlgateBridgeAmount, setDlgateBridgeAmount] = useState("0");
   const [oftHyperBridge, setOFTHyperBridge] = useState(false);
   const [onftHyperBridge, setONFTHyperBridge] = useState(false);
 
@@ -879,7 +879,7 @@ export default function Home({
 
   const handleDlgateMax = () => {
     if (balanceOfDlgate) {
-      setDlgateBridgeAmount(Number(balanceOfDlgate?.formatted));
+      setDlgateBridgeAmount((balanceOfDlgate?.formatted));
     }
   };
 
