00fda486150f3bef7b6b1aae514cdd436d1aac0e
Emre Ulgac <ulgacemre@gmail.com>
2023-08-26 13:30:05 +0000

    fix

--- app/api/newCreate/route.ts
+++ app/api/newCreate/route.ts
@@ -4,10 +4,20 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
   const data = await request.json();
 
+  console.log("data", data);
+
+  const headers = {
+    "Content-Type": "application/json",
+    "x-api-key": process.env.NEXT_PUBLIC_API_KEY,
+  };
+
   const refDataResponse = await axios.post(
     `${process.env.NEXT_PUBLIC_API_NEW_URL}/txs/create`,
     {
       ...data,
+    },
+    {
+      headers,
     }
   );
 
