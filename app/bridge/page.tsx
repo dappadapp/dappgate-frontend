3b0c190a6e29236d01f314ded5b2b1c8dbe9baa2
Emre Ulgac <ulgacemre@gmail.com>
2023-10-11 21:33:58 +0000

    mobile fix

--- app/bridge/page.tsx
+++ app/bridge/page.tsx
@@ -186,86 +186,87 @@ const ScrollBridge: React.FC = ({
   };
 
   return (
-    <div className={"relative w-full h-[100vh] min-h-[800px] overflow-x-hidden"}>
-      <div
-        className={
-          "relative  z-10 w-full min-h-[800px] h-full flex flex-col p-2 align-middle justify-center items-center"
-        }
-      >
-        <h2 className="text-7xl font-bold mb-10 text-white">Welcome to Scroll <br></br>
-        </h2>
-        <h2 className="text-7xl font-bold mb-10 text-white">
-          Native Bridge Tool</h2>
-        <div className="flex space-x-4 mb-5 w-full items-center justify-center mt-5">
-          <ListboxSourceMenu
-            value={networks.filter((network) => network.chainId === 1)[0]}
-            onChange={onChangeSourceChain}
-            options={networks.filter((network) => network.chainId === 5343521)}
-            searchValue={searchTerm}
-            setSearchValue={setSearchTerm}
-            className=" w-full"
-          />
-
-          <CircleSvg onArrowClick={() => { }} isClickable={true} />
-
-          <ListboxSourceMenu
-            value={networks.filter((network) => network.chainId === 534352)[0]}
-            onChange={onChangeSourceChain}
-            options={networks.filter((network) => network.chainId === 1111111111111)}
-            searchValue={searchTerm}
-            setSearchValue={setSearchTerm}
-          />
-        </div>
-        <div className="flex items-center justify-center mb-10 mt-5">
-          <div className=" shadow-md rounded-lg p-6 w-full">
-            <div className="flex items-center justify-between mb-2">
-              <h2 className="text-xl text-white font-semibold">Deposit ETH Amount</h2>
-              <span className="text-xl text-white">Balance: {Number(balanceOfUser?.formatted).toFixed(4)} ETH</span>
-            </div>
-            <div className="flex items-center mt-5">
-
-              <input
-                type="text"
-                value={amount}
-                onChange={(e) => setAmount(e.target.value)}
-                className="bg-black border rounded w-[30em] px-4 py-4 text-white text-[18px] mr-2"
-                placeholder="Enter ETH amount"
-              />
-              <button
-                onClick={handleBridge}
-                className="flex items-center gap-1 bg-blue-500 hover:bg-blue-600 text-white px-4 py-4 rounded disabled:bg-red-500/20 disabled:cursor-not-allowed mt-1"
-                disabled={loading}
-              >
-                Bridge ETH
-                {loading && (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-4 h-4 animate-spin"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
-                    />
-                  </svg>
-                )}
-              </button>
-            </div>
+    <div className="relative w-full h-auto min-h-[800px] overflow-x-hidden">
+    <div className="relative z-10 w-full min-h-[800px] h-full flex flex-col p-2 align-middle justify-center items-center">
+      <h2 className="text-5xl md:text-7xl font-bold mb-2 md:mb-5 text-white">
+        Welcome to Scroll
+      </h2>
+      <h2 className="text-5xl md:text-7xl font-bold mb-5 md:mb-5 text-white">
+       Native Bridge Tool
+      </h2>
+      <div className="flex mb-5 w-full md:w-full items-center justify-center mt-5 mb-5">
+        <ListboxSourceMenu
+          value={networks.filter((network) => network.chainId === 1)[0]}
+          onChange={onChangeSourceChain}
+          options={networks.filter((network) => network.chainId === 5343521)}
+          searchValue={searchTerm}
+          setSearchValue={setSearchTerm}
+          className="w-full "
+        />
+        <CircleSvg onArrowClick={() => {}} isClickable={true} />
+        <ListboxSourceMenu
+          value={networks.filter((network) => network.chainId === 534352)[0]}
+          onChange={onChangeSourceChain}
+          options={networks.filter((network) => network.chainId === 1111111111111)}
+          searchValue={searchTerm}
+          setSearchValue={setSearchTerm}
+          className="w-full"
+        />
+      </div>
+      <div className="flex flex-col items-center justify-center mb-4 md:mb-10">
+        <div className="shadow-md rounded-lg p-10 w-full md:w-auto">
+          <div className="flex flex-col md:flex-row items-center justify-between mb-2">
+            <h2 className="text-lg md:text-xl text-white font-semibold mb-2 md:mb-0">
+              Deposit ETH Amount
+            </h2>
+            <span className="text-lg md:text-xl text-white md:ml-4">
+              Balance: {Number(balanceOfUser?.formatted).toFixed(4)} ETH
+            </span>
+          </div>
+          <div className="flex flex-col md:flex-row items-center mt-4 md:mt-5">
+            <input
+              type="text"
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+              className="bg-black border rounded px-4 py-4 text-white w-full md:w-[30em] mb-2 md:mb-0"
+              placeholder="Enter ETH amount"
+            />
+            <button
+              onClick={handleBridge}
+              className="flex items-center ml-4 bg-blue-500 hover:bg-blue-600 text-white px-4 py-4 rounded disabled:bg-red-500/20 disabled:cursor-not-allowed"
+              disabled={loading}
+            >
+              Bridge ETH
+              {loading && (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-4 h-4 animate-spin"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"
+                  />
+                </svg>
+              )}
+            </button>
           </div>
         </div>
-
-        <div>{/* Official Scroll Bridge Contract is in use */}
-          <div className="text-lg font-semi-bold mb-1 mt-10 text-grey-400"><strong className="text-blue-300 ml-5"> Powered by Scroll</strong> <br></br> </div>
-        </div>
-
-
       </div>
-      <ToastContainer position="top-right" theme="dark" />
+  
+      <div>
+        <div className="text-base md:text-lg font-semibold mb-1 mt-4 md:mt-10 text-grey-400">
+          <strong className="text-blue-300 ml-2 md:ml-5">Powered by Scroll</strong>
+        </div>
+      </div>
     </div>
+    <ToastContainer position="top-right" theme="dark" />
+  </div>
+  
   );
 
 };
