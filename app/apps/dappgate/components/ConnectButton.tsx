f06ee883a161bdd3b1eb4b4f196ce07f5d6d5377
Emre Ulgac <ulgacemre@gmail.com>
2023-11-04 15:50:52 +0000

    fix

--- app/apps/dappgate/components/ConnectButton.tsx
+++ app/apps/dappgate/components/ConnectButton.tsx
@@ -21,7 +21,7 @@ const ConnectButton: React.FC<Props> = ({ pendingTxs }) => {
       className={
         "rounded-lg tracking-wider duration-150 hover:bg-black hover:text-white hover:outline-white outline transition px-3 py-1 sm:px-5 sm:py-2 lg:px-8 lg:py-3.5 text-black font-semibold select-none text-sm sm:text-base"
       }
-      onClick={open}
+      onClick={() => open()}
     >
       {pendingFilter.length > 0 ? (
         <span className="ml-2 text-sm text-black-500 flex justify-center items-center">
